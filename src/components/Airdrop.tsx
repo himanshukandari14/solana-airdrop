2198f5ad61f0a9190c6be15f99d10fb3ea102c9f
himanshukandari14 <himanshukandari14@gmail.com>
2025-01-23 12:56:23 +0530

    added get balance func

--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -1,10 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-nocheck
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useEffect, useState } from "react";
 
 export const Airdrop = () => {
   const wallet = useWallet();
   const { connection } = useConnection();
+  const [balance,setBalance]=useState('');
+
+  const getInfo=async()=>{
+    const info=await connection.getAccountInfo(wallet.publicKey);
+    const balance=info?.lamports || 0;
+    setBalance(balance);
+  }
+
+  useEffect(()=>{
+    getInfo()
+  },[connection, getInfo])
 
   const sendAirdropToUser = async () => {
     try {
@@ -22,6 +34,7 @@ export const Airdrop = () => {
   return (
     <div style={{height:'200px', width:'400px'}}>
       {wallet.publicKey?.toString()}
+      <div>Balance: {balance}</div>
       <input type="text" id="pk" placeholder="Amount" />
       <button onClick={sendAirdropToUser}>Send airdrop</button>
     </div>
